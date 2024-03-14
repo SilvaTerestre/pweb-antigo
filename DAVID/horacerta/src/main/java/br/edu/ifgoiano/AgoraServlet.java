583105f8f578fb2277005189593616c5c17fe43e
silve <silve@DESKTOP-LKU40FP>
2024-03-14 07:12:58 -0300

    teste

--- DAVID/horacerta/src/main/java/br/edu/ifgoiano/AgoraServlet.java
+++ DAVID/horacerta/src/main/java/br/edu/ifgoiano/AgoraServlet.java
@@ -22,6 +22,6 @@ public class AgoraServlet extends HttpServlet {
 		
 		resp.setContentType("text/html;charset=UTF=8");
 		resp.getWriter().print(html);
-		System.out.println("Testandoo o Git");
+		System.out.println("Testando o Git");
 	}
 }
